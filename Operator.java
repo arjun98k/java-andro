491912a92d68b3057ba68b1a918cd9080382d599
arjun98k <arjunkandekar14@gmail.com>
2025-01-13 20:34:14 +0530

    preincrement and postincrement

---
+++ Operator.java
@@ -0,0 +1,22 @@
+class Operator {
+	public static void main(String[] args){
+		// java unary Operator
+		int x = 1;
+        int  y = x++;
+         // int z = 
+		//post increment
+		 //in this expression store first then 
+		 // evalulation of operation is done
+		System.out.println(y);
+		System.out.println(x);
+        // pre increment
+         //in this expression first evalulation done
+		 // operation is done
+
+
+		int a = 1;
+		int b = ++a;
+		System.out.println(b);
+		System.out.println(b);
+	}
+}
