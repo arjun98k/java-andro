004128616b01a20981f6323e9837f9717d360ab5
arjun98k <arjunkandekar14@gmail.com>
2025-01-13 19:18:33 +0530

    datatype in java

---
+++ Data_type.java
@@ -0,0 +1,17 @@
+public class Data_type {
+    public static void main(String[] args) {
+        int a = 98;
+        System.out.println(a);
+    }
+}
+// data type
+/*
+boolean
+Char 
+byte
+short
+int
+long
+float
+double
+*/
