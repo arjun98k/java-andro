072296060ffd76d7bbdb31236d10ff2ead03fa2f
arjun98k <arjunkandekar14@gmail.com>
2025-01-16 19:42:59 +0530

    String method more

---
+++ Stringmethod2.java
@@ -0,0 +1,8 @@
+public class Stringmethod2 {
+    public static void main(String[] args) {
+        String str = "This is tutorialspoint";
+        // System.out.print("The string is: " + str);
+        System.out.println(str.charAt(0));
+        System.out.println(str.startsWith("t"));
+    }
+}
