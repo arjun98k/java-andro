142d3d63eea9d7411082abaa5ed7b7d13976b3e3
arjun98k <arjunkandekar14@gmail.com>
2025-01-16 11:59:40 +0530

    String Method

--- StringMethod.java
+++ StringMethod.java
@@ -3,11 +3,15 @@ import java.util.*;
 class StringMethod {
 	public static void main(String[] args){
 		String Akash = "SaLeKar";
+		String s1 = "Arjun is memepage admin";
 		
 		System.out.println(Akash.length());
 
 		// upper case and lower case method
 		System.out.println(Akash.toUpperCase());
 		System.out.println(Akash.toLowerCase());
+
+		System.out.println(s1.indexOf("memepage"));
 	}
+
 }
