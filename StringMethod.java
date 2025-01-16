956b1cea08fb1d2e1a7d1e313cc23c63b088777c
arjun98k <arjunkandekar14@gmail.com>
2025-01-16 11:43:07 +0530

    String Method

--- StringMethod.java
+++ StringMethod.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 class StringMethod {
 	public static void main(String[] args){
-		String Akash = "salekar";
+		String Akash = "SaLeKar";
 		
 		System.out.println(Akash.length());
+
+		// upper case and lower case method
+		System.out.println(Akash.toUpperCase());
+		System.out.println(Akash.toLowerCase());
 	}
 }
