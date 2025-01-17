fd0af36d5b8019b1a466ad708909629989e24a46
arjun98k <arjunkandekar14@gmail.com>
2025-01-17 13:46:45 +0530

    String Buffer Method

---
+++ Strngbuff.java
@@ -0,0 +1,16 @@
+public class Strngbuff {
+	public static void main(String[] args){
+		StringBuffer s1 = new StringBuffer("Arjun");
+		// Stringbuffer class append
+		s1.append("kandekar");
+		// System.out.println(s1);
+
+		// StringBuffer insert() Method
+		// s1.insert(0,"boss ");
+		// System.out.println(s1);
+
+		//StringBuffer replace() Method
+		s1.replace(0,5,"akash");
+		System.out.println(s1);
+	}
+}
