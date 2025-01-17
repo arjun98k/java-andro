67831a872c6db6cc586fe45f33a754a5cc1f296b
arjun98k <arjunkandekar14@gmail.com>
2025-01-17 13:05:04 +0530

    Strings method

--- Stringmethod2.java
+++ Stringmethod2.java
@@ -4,5 +4,10 @@ public class Stringmethod2 {
         // System.out.print("The string is: " + str);
         System.out.println(str.charAt(0));
         System.out.println(str.startsWith("t"));
+        System.out.println(str.endsWith("int"));
+        System.out.println(str.trim());
+
+        // java String charAt Method
+        System.out.println(str.charAt(1));
     }
 }
