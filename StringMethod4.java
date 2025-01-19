3e737216503a93e07c3c073b31922514f3632a7f
arjun98k <arjunkandekar14@gmail.com>
2025-01-19 13:57:42 +0530

    String Method

--- StringMethod4.java
+++ StringMethod4.java
@@ -1,8 +1,16 @@
 class StringMethod4 {
 	public static void main(String[] args){
-		String str1 = "aditya";
+		String str1 = "   aditya    ";
 		String str2 = "amey";
-		System.out.println(str1.equals(str2));
+		// System.out.println(str1.equals(str2));
+    System.out.println("hii");
+    // substring method
+    // System.out.println(str1.substring(1,3));
+    // contains method
+    System.out.println(str1.contains("di")); // true because di present in str1 Aditya
+
+    // trim() remove start and space from string
+    System.out.println(str1.trim());
 
 	}
 }
