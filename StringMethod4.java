12639a204de1e814d384d8791d84ed62b1c3a0e8
arjun98k <arjunkandekar14@gmail.com>
2025-01-20 14:19:21 +0530

    StringMethod4

--- StringMethod4.java
+++ StringMethod4.java
@@ -1,25 +1,29 @@
 import java.util.Arrays;
 class StringMethod4 {
 	public static void main(String[] args){
-		String str1 = "   aditya    ";
+		String str1 = "aditya";
 		String str2 = "amey";
 		// System.out.println(str1.equals(str2));
-    System.out.println("hii");
+    // System.out.println("hii");
     // substring method
     // System.out.println(str1.substring(1,3));
     // contains method
-    System.out.println(str1.contains("di")); // true because di present in str1 Aditya
+    // System.out.println(str1.contains("di")); // true because di present in str1 Aditya
 
     // trim() remove start and space from string
 
-    System.out.println(str1.trim());
+    // System.out.println(str1.trim());
 
     //String equalIgnoreCase(String anotherString)
     String Str3 = "Arjun";
     String Str4 = "Arjun, Akash, Aditya";
      String[] ak = Str4.split(",");
+     char[] am = str1.toCharArray();
  
-  System.out.println(Arrays.toString(ak));
+  // System.out.println(Arrays.toString(ak));
+ for (char a : am ) {
+     System.out.println(a);
+ }
     
 
 	}
