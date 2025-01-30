13c7fd71d16947256d827aa12bd5718c55302f4a
arjun98k <arjunkandekar14@gmail.com>
2025-01-30 13:48:25 +0530

    Strings

--- Stringmethod2.java
+++ Stringmethod2.java
@@ -2,12 +2,16 @@ public class Stringmethod2 {
     public static void main(String[] args) {
         String str = "This is tutorialspoint";
         // System.out.print("The string is: " + str);
-        System.out.println(str.charAt(0));
-        System.out.println(str.startsWith("t"));
-        System.out.println(str.endsWith("int"));
-        System.out.println(str.trim());
+        // System.out.println(str.charAt(0));
+        // System.out.println(str.startsWith("t"));
+        // System.out.println(str.endsWith("int"));
+        // System.out.println(str.trim());
 
-        // java String charAt Method
-        System.out.println(str.charAt(1));
+        // // java String charAt Method
+        // System.out.println(str.charAt(1));
+
+        // java String.contains method
+       String str2 = "i am that sky who has no limit";
+        System.out.println(str2.contains("arjun"));
     }
 }
