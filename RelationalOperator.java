bd5e0d85ca69a7ecd196e53fc8635d30ad08566d
arjun98k <arjunkandekar14@gmail.com>
2025-01-30 12:35:57 +0530

    relational operator in java

---
+++ RelationalOperator.java
@@ -0,0 +1,11 @@
+class RelationalOperator{
+	public static void main(String[] args){
+		char a = 'a';
+		char b = 'z';
+		System.out.println('z'+ 0);
+		System.out.println('a'+ 0);
+		boolean d = a < b; // 97 < 122 = true
+		System.out.println(d);
+
+	}
+}
