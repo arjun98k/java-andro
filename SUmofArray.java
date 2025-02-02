7c8d0e01f18c664de8a74696bcf890796b632ed4
arjun98k <arjunkandekar14@gmail.com>
2025-02-02 13:24:46 +0530

    Sum of arrays

---
+++ SUmofArray.java
@@ -0,0 +1,9 @@
+class SUmofArray {
+	public static void main(String[] args){
+		int arr [] = {10,20,30,40,50};
+		int res= 0;
+		for (int a :arr ) {
+			res+=a;
+		} System.out.println("Total sum of Arrays" + " "+ res);
+	}
+}
