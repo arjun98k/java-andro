69d99030cacf259d00ccc16c8add771b0fb23d05
arjun98k <arjunkandekar14@gmail.com>
2025-02-02 13:38:05 +0530

    2D arrays print

---
+++ TwoDarray1.java
@@ -0,0 +1,15 @@
+public class TwoDarray1 {
+    public static void main(String[] args) {
+        int arr [][] = {
+            {11,12,14},
+            {16,17,18},
+            {19,20,21}
+        };
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i] [j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
