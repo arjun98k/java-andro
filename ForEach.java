d35712a71d5cf721a294e60d82d3ff2c1ca9d56f
arjun98k <arjunkandekar14@gmail.com>
2025-02-02 11:34:17 +0530

    finding great element in arrays and finding element arrays solved

--- ForEach.java
+++ ForEach.java
@@ -1,8 +1,19 @@
 public class ForEach {
     public static void main(String[] args) {
-    int [] arr1 = {11,13,34,43};
-           for(int a: arr1){
-            System.out.println(a);
-           }
+    int [] arr1 = {11,13,66,34,222,43};
+        //    for(int a: arr1){
+        //    if (a == 34){
+        //    System.out.println("elements is find" +" " +a);
+        //          }
+        //    }
+        
+        int ans = 0;
+        for (int a : arr1) {
+            if (a > ans) {
+                ans = a;
+            }
+            
+        }
+        System.out.println(ans);
     }
 }
