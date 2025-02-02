c1bc54c8185be2b0a7f191da652fa04d6a5d6a80
arjun98k <arjunkandekar14@gmail.com>
2025-02-02 11:23:28 +0530

    for each loop mostly use for collection of same items

---
+++ ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+    int [] arr1 = {11,13,34,43};
+           for(int a: arr1){
+            System.out.println(a);
+           }
+    }
+}
