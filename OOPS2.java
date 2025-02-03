92991f7eef5c15bec78c1362e41d847478ec8e68
arjun98k <arjunkandekar14@gmail.com>
2025-02-03 14:25:10 +0530

    polymorphism example

---
+++ OOPS2.java
@@ -0,0 +1,22 @@
+
+class Animal{
+    String Animal_sound = "bark";
+}
+
+class Dog extends Animal {
+
+    
+}
+
+
+class OOPS2 {
+
+
+    public static void main(String[] args) {
+        
+    Dog sc = new Dog();
+    //polymorphism
+    System.out.println(sc.Animal_sound);
+
+    }
+}
