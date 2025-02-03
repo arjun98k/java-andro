415d0b210c8f31c549a33b454b566485e391d483
arjun98k <arjunkandekar14@gmail.com>
2025-02-03 14:07:58 +0530

    foreach loop

--- ForEach.java
+++ ForEach.java
@@ -1,12 +1,8 @@
 public class ForEach {
     public static void main(String[] args) {
     int [] arr1 = {11,13,66,34,222,43};
-        //    for(int a: arr1){
-        //    if (a == 34){
-        //    System.out.println("elements is find" +" " +a);
-        //          }
-        //    }
-        
+       
+
         int ans = 0;
         for (int a : arr1) {
             if (a > ans) {
@@ -15,5 +11,12 @@ public class ForEach {
             
         }
         System.out.println(ans);
+
+
+         //    for(int a: arr1){
+        //    if (a == 34){
+        //    System.out.println("elements is find" +" " +a);
+        //          }
+        //    }
     }
 }
