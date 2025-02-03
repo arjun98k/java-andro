cc0c3ad6e521cd8fe53ec220f35ecd93c4ceb7b3
arjun98k <arjunkandekar14@gmail.com>
2025-02-03 14:50:39 +0530

    polymorphism

---
+++ OOPS3.java
@@ -0,0 +1,33 @@
+class Animals{
+	void makesound(){
+		System.out.println("sound sound");
+	}
+	
+}
+class Dog extends Animals{
+	void makesound(){
+	System.out.println("bark woof");
+}
+}
+class Cat extends Animals{
+	void makesound(){
+	System.out.println("meow meow");
+}
+}
+
+
+
+
+
+
+class OOPS3 {
+	public static void main(String[] args){
+
+		Animals dog = new Dog();
+		Animals cat = new Cat(); 
+		
+		dog.makesound();
+		cat.makesound();
+
+	}
+}
