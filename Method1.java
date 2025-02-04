0f787c3ff91f16f58d1014590330e764649a5403
arjun98k <arjunkandekar14@gmail.com>
2025-02-04 10:48:38 +0530

    Method with param string int and many more

--- Method1.java
+++ Method1.java
@@ -8,10 +8,38 @@ class TP1{
 }
 
 
+class TP2  {
+	String Uppercut(String a){
+		String b = a.toUpperCase();
+		return b;
+	}
+}
+
+class TP3 {
+	 String GreetMessage(){
+	String	d = "good morning";
+	System.out.println(d);
+          return d;
+	} 
+}
+
 class Method1 {
+    private String Adi(){
+		String a = "hii";
+		System.out.println(a);
+		return a;
+
+	}
+
 	public static void main(String[] args){
 		TP1 ac = new TP1();
+		TP2 bc = new TP2();
+		TP3 dc = new TP3();
 		System.out.println(ac.Sum(10,20));
+		System.out.println(bc.Uppercut("akash"));
+		dc.GreetMessage();
+		Method1 xz = new Method1();
+		xz.Adi();
 
 	}
 }
