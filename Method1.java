40333f4d833fa9a59525d1cfcdaa38120fda729e
arjun98k <arjunkandekar14@gmail.com>
2025-02-04 10:17:18 +0530

    Method with param

---
+++ Method1.java
@@ -0,0 +1,17 @@
+
+
+
+class TP1{
+	int Sum(int a , int b){
+		return a+b;
+	}
+}
+
+
+class Method1 {
+	public static void main(String[] args){
+		TP1 ac = new TP1();
+		System.out.println(ac.Sum(10,20));
+
+	}
+}
