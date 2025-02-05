30d0c5fb4ba23475fb8fad0855696d50bd7b5847
arjun98k <arjunkandekar14@gmail.com>
2025-02-05 11:28:49 +0530

    default and parameterised constructor

--- Constructor1.java
+++ Constructor1.java
@@ -4,11 +4,18 @@ class Default1{
 	public Default1(){
 	System.out.println("called default Constructor1");
 }
+
+public Default1(String name){
+	// this.name = name;
+	System.out.println(name);
+
+}
 }
 
 class Constructor1{
 public static void main(String[] args){
     Default1 obj = new Default1();
+      Default1 obj1 = new Default1("Arjun");
 
 }
 }
