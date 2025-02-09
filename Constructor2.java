4b746c0575b0f24c20f73d39d621955f9dae430c
arjun98k <arjunkandekar14@gmail.com>
2025-02-09 12:53:42 +0530

    default constructor

---
+++ Constructor2.java
@@ -0,0 +1,16 @@
+class  Constructor2 {
+	public static void main(String[] args){
+     Const1 obj1 = new Const1();
+	}
+}
+
+
+class Const1{
+	int age;
+	String name;
+	String job;
+
+	public Const1(){
+		System.out.println("hey we are default const");
+	}
+}
