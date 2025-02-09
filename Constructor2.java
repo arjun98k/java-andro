0fbd74fda7a9ae2a9b02b9385267c3e5534ec33f
arjun98k <arjunkandekar14@gmail.com>
2025-02-09 12:59:49 +0530

    default constructor and parameterised constructor

--- Constructor2.java
+++ Constructor2.java
@@ -1,6 +1,10 @@
 class  Constructor2 {
 	public static void main(String[] args){
      Const1 obj1 = new Const1();
+     Const1 obj2 = new Const1(11,"arjun","sde");
+     System.out.println(obj2.name);
+       System.out.println(obj2.age);
+         System.out.println(obj2.job);
 	}
 }
 
@@ -12,5 +16,11 @@ class Const1{
 
 	public Const1(){
 		System.out.println("hey we are default const");
+	}
+
+	public Const1(int age , String name, String job){
+		this.name = name;
+		this.age = age;
+		this.job = job;
 	}
 }
