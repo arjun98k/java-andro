d30e290360fd0ef6a2bf286e6d346a9652e97af1
arjun98k <arjunkandekar14@gmail.com>
2025-03-02 23:59:25 +0530

    Strim

---
+++ PalindromeString.java
@@ -0,0 +1,10 @@
+public class PalindromeString {
+
+    public static Boolean isPalindrome(String str){
+        String rev = new StringBuilder(str).reverse().toString();
+        return str.equals(rev);
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("radar"));
+    }
+}
