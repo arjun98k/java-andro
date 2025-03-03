76281fce4eead18182892c0b71b3302974079012
arjun98k <arjunkandekar14@gmail.com>
2025-03-03 13:16:36 +0530

    palindrome string

--- PalindromeString.java
+++ PalindromeString.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class PalindromeString {
 
     public static Boolean isPalindrome(String str){
+    
         String rev = new StringBuilder(str).reverse().toString();
+        rev.toLowerCase();
         return str.equals(rev);
     }
     public static void main(String[] args) {
-        System.out.println(isPalindrome("radar".toLowerCase()));
+        System.out.println("Enter String you have check palindrome ");
+        Scanner ac = new Scanner(System.in);
+        String input = ac.nextLine();
+        System.out.println(isPalindrome(input));
     }
 }
