f0e4c2f7bcde0b40bb9b042ba61d70baa14d77ec
arjun98k <arjunkandekar14@gmail.com>
2025-03-05 11:48:46 +0530

    Stringmethod

--- StringMethod1.java
+++ StringMethod1.java
@@ -7,7 +7,13 @@ public class StringMethod1 {
 // Usage: To access a specific character in a string.
 
         char ak = str1.charAt(4);
-        System.out.println(ak);
+        // System.out.println(ak);
+
+//         2. length()
+// Description: Returns the length (number of characters) of the string.
+// Usage: To find how many characters are in a string.
         
+           System.out.println(str1.length());
+
     }
 }
