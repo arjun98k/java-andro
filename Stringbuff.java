1c9f1155e8a1414b5cd9f1ab0a52b80470b17e39
arjun98k <arjunkandekar14@gmail.com>
2025-03-05 10:12:24 +0530

    String builder

--- Stringbuff.java
+++ Stringbuff.java
@@ -2,7 +2,13 @@ class Stringbuff{
 	public static void main(String[] args){
 		StringBuilder str1 = new StringBuilder("Arjun");
 		System.out.println(str1);
-		System.out.println(str1.append('a'));
+		for (int i = 0; i < 10; i++) {
+			str1.append('a');
+
+		}
+
+		System.out.println(str1);
+		
 		
 	}
 }
