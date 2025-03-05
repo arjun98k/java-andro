54cc39909bf6dad95ab021eb347422111509645c
arjun98k <arjunkandekar14@gmail.com>
2025-03-05 12:18:17 +0530

    Stringmethod

--- StringMethod1.java
+++ StringMethod1.java
@@ -13,18 +13,24 @@ public class StringMethod1 {
 // Description: Returns the length (number of characters) of the string.
 // Usage: To find how many characters are in a string.
         
-           System.out.println(str1.length());
+        //    System.out.println(str1.length());
 
 //            3. substring(int beginIndex, int endIndex)
 // Description: Returns a new string that is a substring of the original string, starting at beginIndex and ending before endIndex.
 // Usage: To extract a part of the string.
             
-System.out.println(str1.substring(1, 3));
+// System.out.println(str1.substring(1, 3));
 // 6. trim()
 
 // Description: Removes leading and trailing whitespace from the string.
 // Usage: To clean up spaces from the beginning and end of the string.
-     System.out.println(str1.trim());
+    //  System.out.println(str1.trim());
+
+//      7. replace(char oldChar, char newChar)
+// Description: Replaces all occurrences of oldChar with newChar in the string.
+// Usage: To replace specific characters in a string.
+
+   System.out.println(str1.replace('A', 'k'));
 
     }
 }
