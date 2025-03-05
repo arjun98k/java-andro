29e180e732d5183c98c9b27b781285a1133eb2dd
arjun98k <arjunkandekar14@gmail.com>
2025-03-05 11:24:30 +0530

    Stringmethod

---
+++ StringMethod1.java
@@ -0,0 +1,13 @@
+public class StringMethod1 {
+    public static void main(String[] args) {
+        String str1 = "Arjun";
+
+//         1. charAt(int index)
+// Description: Returns the character at the specified index of the string.
+// Usage: To access a specific character in a string.
+
+        char ak = str1.charAt(4);
+        System.out.println(ak);
+        
+    }
+}
